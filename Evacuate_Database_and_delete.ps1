b4be4034d3afb8240ee9978a8e28cecd2673af5e
DiggaTS <63748159+DiggaTS@users.noreply.github.com>
2023-11-21 09:11:51 +0100

    Update Evacuate_Database_and_delete.ps1

--- Evacuate_Database_and_delete.ps1
+++ Evacuate_Database_and_delete.ps1
@@ -28,28 +28,50 @@ param(
     [string]$DatabaseDestination=(Read-Host "Bitte Zieldatenbank eingeben")
 )
 
-
 write-Host "Es wird von: " $DatabaseSource "zu:" $DatabaseDestination "kopiert!" -BackgroundColor Green -ForegroundColor Black
-write-Host "Warte 10 Sekunden" -BackgroundColor Green -ForegroundColor Black
-start-sleep -Seconds 10
-#Arbitation databse
-get-mailbox -database $DatabaseSource  -Arbitration | New-MoveRequest -TargetDatabase $DatabaseDestination
-#Discovery Mailbox
-get-mailbox -Database $DatabaseSource -RecipientTypeDetails DiscoveryMailbox | New-MoveRequest -TargetDatabase $DatabaseDestination
-#User tool mailboxes
-Get-Mailbox -database $DatabaseSource -RecipientTypeDetails SharedMailbox, RoomMailbox, EquipmentMailbox | New-MoveRequest -TargetDatabase $DatabaseDestination
-#User mailboxes
-Get-Mailbox -database $DatabaseSource | New-MoveRequest -TargetDatabase $DatabaseDestination
-#Public Folder
-get-mailbox -Database $DatabaseSource -PublicFolder | New-MoveRequest -TargetDatabase $DatabaseDestination 
-#Audit log
-get-mailbox -Database $DatabaseSource -AuditLog | New-MoveRequest -TargetDatabase $DatabaseDestination
+$Antwort = Read-Host -Prompt "Möchtest du fortfahren? (Ja/Nein)"
+
+if ($Antwort -eq "Ja" -or $antwort -eq "ja") {
+	#Arbitation databse
+	get-mailbox -database $DatabaseSource  -Arbitration | New-MoveRequest -TargetDatabase $DatabaseDestination
+	#Discovery Mailbox
+	get-mailbox -Database $DatabaseSource -RecipientTypeDetails DiscoveryMailbox | New-MoveRequest -TargetDatabase $DatabaseDestination
+	#User tool mailboxes
+	Get-Mailbox -database $DatabaseSource -RecipientTypeDetails SharedMailbox, RoomMailbox, EquipmentMailbox | New-MoveRequest -TargetDatabase $DatabaseDestination
+	#User mailboxes
+	Get-Mailbox -database $DatabaseSource | New-MoveRequest -TargetDatabase $DatabaseDestination
+	#Public Folder
+	get-mailbox -Database $DatabaseSource -PublicFolder | New-MoveRequest -TargetDatabase $DatabaseDestination 
+	#Audit log
+	get-mailbox -Database $DatabaseSource -AuditLog | New-MoveRequest -TargetDatabase $DatabaseDestination
+	}
+elseif ($antwort -eq "Nein" -or $antwort -eq "nein" -or $antwort -eq "N" -or $antwort -eq "n" -or [string]::IsNullOrEmpty($antwort)) {
+
+    	Write-Host "Du hast 'Nein' gewählt oder nichts eingegeben. Abbruch des Skripts."
+	exit
+}
+else {
+    Write-Host "Ungültige Eingabe. Bitte 'Ja' oder 'Nein' eingeben."
+}
 
 
-write-host "Lösche Mailboximport Requests" -ForegroundColor Yellow 
-Get-MailboxImportRequest -Status Completed | Remove-MailboxImportRequest -force
-Get-MailboxImportRequest -Status Failed | Remove-MailboxImportRequest  -force
-Get-MailboxImportRequest -Status CompletedWithWarning | Remove-MailboxImportRequest -force
+write-Host "Achtung es werden die ImportRequests gelöscht" -BackgroundColor Green -ForegroundColor Black
+$Antwort = Read-Host -Prompt "Möchtest du fortfahren? (Ja/Nein)"
+
+if ($Antwort -eq "Ja" -or $antwort -eq "ja") {
+	Get-MailboxImportRequest -Status Completed | Remove-MailboxImportRequest -force
+	Get-MailboxImportRequest -Status Failed | Remove-MailboxImportRequest  -force
+	Get-MailboxImportRequest -Status CompletedWithWarning | Remove-MailboxImportRequest -force
+	}
+elseif ($antwort -eq "Nein" -or $antwort -eq "nein" -or $antwort -eq "N" -or $antwort -eq "n" -or [string]::IsNullOrEmpty($antwort)) {
+
+    	Write-Host "Du hast 'Nein' gewählt oder nichts eingegeben. Abbruch des Skripts."
+	exit
+}
+else {
+    Write-Host "Ungültige Eingabe. Bitte 'Ja' oder 'Nein' eingeben."
+}
+
 
 
 $Moverequest = Get-MoveRequest
@@ -59,8 +81,24 @@ while ((Get-Moverequest | where-object {$PSItem.Status -ne "Completed" })){
     start-sleep -Seconds 60
 }
 
-
 get-moverequest | Remove-MoveRequest -Force
 write-host "Lösche Quelldatenbank ab"
 start-sleep -Seconds 30
-Get-MailboxDatabase -Identity $DatabaseSource | Remove-MailboxDatabase 
+
+write-Host "Achtung es wird die Datenbank gelöscht" -BackgroundColor Green -ForegroundColor Black
+$Antwort = Read-Host -Prompt "Möchtest du fortfahren? (Ja/Nein)"
+
+if ($Antwort -eq "Ja" -or $antwort -eq "ja") {
+	Get-MailboxDatabase -Identity $DatabaseSource | Remove-MailboxDatabase 
+	}
+elseif ($antwort -eq "Nein" -or $antwort -eq "nein" -or $antwort -eq "N" -or $antwort -eq "n" -or [string]::IsNullOrEmpty($antwort)) {
+
+    	Write-Host "Du hast 'Nein' gewählt oder nichts eingegeben. Abbruch des Skripts."
+	exit
+}
+else {
+    Write-Host "Ungültige Eingabe. Bitte 'Ja' oder 'Nein' eingeben."
+}
+
+write-Host "Bitte .EDB der Datenbank löschen" -BackgroundColor Green -ForegroundColor Black
+
